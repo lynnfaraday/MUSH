c0a79cf8527652995f085fdfef75ebefa49d404d
lomeara <lomeara@e2dc93ee-7d2d-0410-af30-bd5c5d140aa9>
2010-07-18 03:20:33 +0000

    Added connect command and rudimentary player tracking to WHO. Split WHO into a separate module.
    git-svn-id: https://faramushcode.googlecode.com/svn/trunk@507 e2dc93ee-7d2d-0410-af30-bd5c5d140aa9

--- aresmush/testMain.py
+++ aresmush/testMain.py
@@ -2,10 +2,14 @@ import unittest
 
 from aresmush.tests.commandTests import CommandTests
 from aresmush.tests.moduleManagerTests import ModuleManagerTests
+from aresmush.tests.whoTests import WhoTests
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(CommandTests)
     unittest.TextTestRunner(verbosity=2).run(suite)
     
     suite = unittest.TestLoader().loadTestsFromTestCase(ModuleManagerTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    
+    suite = unittest.TestLoader().loadTestsFromTestCase(WhoTests)
     unittest.TextTestRunner(verbosity=2).run(suite)
